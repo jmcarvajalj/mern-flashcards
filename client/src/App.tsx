7c1be51f86ba2555096175034b792f4c20305155
Jose Miguel Carvajal Jimenez <josemcj07@gmail.com>
2023-07-22 16:32:17 -0500

    a lot of changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,66 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [title, setTitle] = useState("")
+  const [decks, setDecks] = useState([])
+
+
+  async function handleCreateDeck(e: React.FormEvent) {
+    e.preventDefault()
+    const response = await fetch("http://localhost:5000/decks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title: title
+      })
+    })
+    const deck = await response.json()
+    setDecks([...decks, deck])
+    setTitle("")
+  }
+
+  async function handleDeleteDeck(deckId: string) {
+    await fetch(`http://localhost:5000/decks/${deckId}`, {
+      method: "DELETE"
+    })
+    setDecks(decks.filter(deck => deck._id !== deckId))
+  }
+
+  useEffect(() => {
+    async function fetchDecks() {
+      const response = await fetch("http://localhost:5000/decks")
+      const newDecks = await response.json()
+      setDecks(newDecks)
+    }
+    fetchDecks()
+  }, [])
+
   return (
     <div className="App">
-      <form action="">
-
-      </form>      
+      <div className="decks">
+        {decks.map((deck) => (
+          <li key={deck._id}>
+            <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
+            {deck.title}
+          </li>
+        ))}
+      </div>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input
+          type="text"
+          id="deck-title"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value)
+          }}
+        />
+        <button>Create Deck</button>
+      </form>
     </div>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv"
 config()
 import express, { Request, Response } from "express"
 import mongoose from "mongoose"
-
+import cors from "cors"
 import Deck from "./models/Deck"
 
 
@@ -10,7 +10,13 @@ const PORT = 5000
 
 const app = express()
 
+app.use(cors({ origin: "*" }))
 app.use(express.json())
+
+app.get("/decks", async (req: Request, res: Response) => {
+    const decks = await Deck.find()
+    res.json(decks)
+})
 
 app.post("/decks", async (req: Request, res: Response) => {
     const newDeck = new Deck({
@@ -18,6 +24,12 @@ app.post("/decks", async (req: Request, res: Response) => {
     })
     const createdDeck = await newDeck.save()
     res.json(createdDeck)
+})
+
+app.delete("/decks/:deckId", async (req: Request, res: Response) => {
+    const deckId = req.params.deckId
+    const deck = await Deck.findByIdAndDelete(deckId)
+    res.json(deck)
 })
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
